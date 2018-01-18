5df23b324796b16ee4ac61e0615e2bb96d78eafd
Karthik Prabu Elangovan (BAS) <KA281619@wipro.com>
2018-01-18 12:57:18 +0530

    Calendar

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -14,24 +14,7 @@ export class CalendarPage {
   uiCalendar : any;
   currentMonth : any = moment();
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-
-
-    this.calendarOptions = {
-      editable: true,
-      eventLimit: false,
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay,listMonth'
-      },
-      events: [{
-        title: 'New event',
-        start: '2018-01-01',
-        end: '2018-01-10',
-      }]
-    };
-
-    this.frameCalendar(0);
+    this.uiCalendar = this.frameCalendar();
 }
   
 
@@ -39,19 +22,15 @@ export class CalendarPage {
     console.log('ionViewDidLoad CalendarPage');
   }
 
-  frameCalendar(i){
-     this.uiCalendar = this.frameSwiper();
-     console.log(this.uiCalendar);
-  }
 
   prevMonth(){
     this.currentMonth = this.currentMonth.subtract(1, 'months');
-    this.uiCalendar = this.frameSwiper();
+    this.uiCalendar = this.frameCalendar();
   }
 
   nextMonth(){
     this.currentMonth = this.currentMonth.add(1, 'months');
-    this.uiCalendar =  this.frameSwiper();
+    this.uiCalendar =  this.frameCalendar();
   }
 
     zellerAlgo(num) {
@@ -74,42 +53,8 @@ export class CalendarPage {
         return calendar;
     };
 
-    frameSwiper() {
+    frameCalendar() {
         var dal = this.currentMonth;
         return this.zellerAlgo(dal);
     };
 }
-
-//  zellerAlgo(num) {
-//         var startWeek = moment(num).startOf('month').week();
-//         var endWeek = moment(num).endOf('month').week();
-//         if(endWeek-startWeek < 0){
-//             endWeek = endWeek + moment(num).weeksInYear();
-//         }
-//         var calendar = []
-//         for (var week = startWeek; week <= endWeek; week++) {
-//             var daysarr = [];
-//             for (var l = 0; l < 7; l++) {
-//                 daysarr.push(moment(num).week(week).startOf('week').clone().add(l, 'day'))
-//             }
-//             calendar.push({
-//                 week: week,
-//                 days: daysarr
-//             })
-//         }
-//         return calendar;
-//     };
-
-//     frameSwiper() {
-//         var value = [];
-//         for (var i = 11; i >= 0; i--) {
-//             var dal = moment().subtract(i, 'months');
-//             var valu = this.zellerAlgo(dal);
-//             value.push({
-//                 "ind": i,
-//                 "momentOjb": dal,
-//                 "calendar": valu
-//             });
-//         }
-//         return value;
-//     };
